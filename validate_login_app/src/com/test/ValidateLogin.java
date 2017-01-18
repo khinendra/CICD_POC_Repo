d635d1473b5742a4e6096191c3c5bf4277266b72
KH255139 <KH255139@L-156058504.wipro.com>
2017-01-18 12:09:33 -0600

    baseline

---
+++ validate_login_app/src/com/test/ValidateLogin.java
@@ -0,0 +1,40 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ValidateLogin {
+	WebDriver driver;
+
+@Test
+public void testLogin() {
+	WebElement userText = driver.findElement(By.name("username"));
+	WebElement passwordText = driver.findElement(By.name("password"));
+	WebElement submit = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
+	userText.sendKeys("admin");
+	passwordText.sendKeys("admin");
+	submit.click();
+}
+@BeforeTest
+public void beforeTest() {
+	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get("http://localhost:8080/WebExample/");
+}
+
+@AfterTest
+public void afterTest() {
+	try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	driver.close();
+}
+}
