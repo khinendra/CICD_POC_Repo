258d916070f8f12a6d6ee37f2c9359331fbb0239
KH255139 <KH255139@L-156058504.wipro.com>
2017-01-22 19:39:53 -0600

    added

--- validate_login_app/src/com/test/ValidateLogin.java
+++ validate_login_app/src/com/test/ValidateLogin.java
@@ -1,5 +1,11 @@
 package com.test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +17,7 @@ import org.testng.annotations.Test;
 
 public class ValidateLogin {
 	WebDriver driver;
+	String appURL2;
 
 @Test
 @Parameters({"user","password"})
@@ -20,24 +27,44 @@ public void testLogin(String user, String password) {
 	String driverLocation=System.getProperty("driver.location");
 	String jenUsername=System.getProperty("username");
 	String jenPassword=System.getProperty("password");
-	String confFile = System.getProperty("conf.file");
+	
 	System.out.println("App URl :"+appURL);
-	System.out.println("driver and config file Location :"+driverLocation+"&"+confFile);
+	System.out.println("driver and config file Location :"+driverLocation);
 	System.out.println("Jenkins UserName & password is:"+jenUsername+"&"+jenPassword);
+	
 	
 	WebElement userText = driver.findElement(By.name("username"));
 	WebElement passwordText = driver.findElement(By.name("password"));
 	WebElement submit = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
-	userText.sendKeys(user);
-	passwordText.sendKeys(password);
+	//userText.sendKeys(user);
+	//passwordText.sendKeys(password);
+	userText.sendKeys(jenUsername);
+	passwordText.sendKeys(jenPassword);
 	submit.click();
 }
 @BeforeTest
 @Parameters({"url"})
 public void beforeTest(String url) {
+	String confFile = System.getProperty("conf.file");
+	File file = new File(confFile);
+	FileInputStream fileInput = null;
+	//String appURL2;
+	try {
+		fileInput = new FileInputStream(file);
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+	Properties prop = new Properties();	
+	//load properties file
+	try {
+		prop.load(fileInput);
+		appURL2 = prop.getProperty("application.url");
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver.exe");
 	driver = new ChromeDriver();
-	driver.get(url);
+	driver.get(appURL2);
 }
 
 @AfterTest
