b64d276280b2a13e3de8a1571fbe03273d228865
Khinendra Kumar Bisen (BAS) <KH255139@wipro.com>
2017-02-20 20:16:04 -0600

    message is good

--- WebExample/src/com/service/LoginServlet.java
+++ WebExample/src/com/service/LoginServlet.java
@@ -43,7 +43,7 @@ public class LoginServlet extends HttpServlet {
 			rd = request.getRequestDispatcher("./success.jsp");
 		}else{
 			message = "failed to login. 777998888999998888";
-			out.write("<html><body><h3>Login failed. Please check your id/password </h3></body></html>");
+			out.write("<html><body><h3>Login failed. Please check your id/password. </h3></body></html>");
 			rd = request.getRequestDispatcher("failure.jsp");
 		}
 		
